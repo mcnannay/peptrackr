58f20746f455770f6ef9d04dac062e968ec79af7
mcnannay <mcnannay@gmail.com>
2025-10-26 12:01:45 -0700

    Delete backend directory

--- backend/app/models_kv.py
+++
@@ -1,6 +0,0 @@
-from typing import Optional, Any
-from sqlmodel import SQLModel, Field, Column, JSON
-
-class KV(SQLModel, table=True):
-    key: str = Field(primary_key=True)
-    value: Any = Field(sa_column=Column(JSON))
